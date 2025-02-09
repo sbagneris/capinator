a06d2183265b49d39c520cc0b33f65fb5074e075
Stephane Bagneris <sbagneris@Stephanes-Mac-Pro.local>
2025-02-09 02:13:42 -0800

    it does things now

--- digikey_cart.py
+++ digikey_cart.py
@@ -1,43 +1,19 @@
 #!/usr/bin/env python3
 
-# import os
-# from pathlib import Path
-
-# import digikey
-# from digikey.v3.productinformation import KeywordSearchRequest
-# from digikey.v3.batchproductdetails import BatchProductDetailsRequest
-
-# CACHE_DIR = Path('.cache/')
-
-# os.environ['DIGIKEY_CLIENT_ID'] = 'exmEWdjlz2bgUAtBNz0zDMC89LZ0gFdg'
-# os.environ['DIGIKEY_CLIENT_SECRET'] = 'YuxtSiWGQp0KHAMj'
-# os.environ['DIGIKEY_CLIENT_SANDBOX'] = 'False'
-# os.environ['DIGIKEY_STORAGE_PATH'] = CACHE_DIR
-
-# # Query product number
-# dkpn = '296-6501-1-ND'
-# part = digikey.product_details(dkpn)
-
-# # Search for parts
-# search_request = KeywordSearchRequest(keywords='CRCW080510K0FKEA', record_count=10)
-# result = digikey.keyword_search(body=search_request)
-
-# # Only if BatchProductDetails endpoint is explicitly enabled
-# # Search for Batch of Parts/Product
-# mpn_list = ["0ZCK0050FF2E", "LR1F1K0"] #Length upto 50
-# batch_request = BatchProductDetailsRequest(products=mpn_list)
-# part_results = digikey.batch_product_details(body=batch_request)
-
 import csv
-import requests
+import json
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
+from pprint import pprint
+
+# Mouser API configuration
+MOUSER_API_KEY = "e74ce92e-fc89-49af-8111-37f23c522a42"
+MOUSER_API_BASE = "https://api.mouser.com/api/v1"
 
 # Digi-Key API configuration
 API_BASE = "https://api.digikey.com"
 CLIENT_ID = "exmEWdjlz2bgUAtBNz0zDMC89LZ0gFdg"
 CLIENT_SECRET = "YuxtSiWGQp0KHAMj"
-REDIRECT_URI = "https://localhost:8080/digikey-callback"  # Update in Dev Portal
 
 # API endpoints
 SEARCH_API = "/products/v4/search/keyword"
@@ -46,57 +22,72 @@ CATEGORIES = "/products/v4/search/categories"
 
 def authenticate():
     """Authenticate with Digi-Key API using OAuth2"""
-    oauth = OAuth2Session(client=BackendApplicationClient(client_id=CLIENT_ID), redirect_uri=REDIRECT_URI)
+    oauth = OAuth2Session(client=BackendApplicationClient(client_id=CLIENT_ID))
     oauth.fetch_token(token_url=f"{API_BASE}/v1/oauth2/token", client_secret=CLIENT_SECRET)
     return oauth
 
 def search_capacitor(session, params):
     """Search for capacitors using Digi-Key API"""
-    # filters = {
-    #     "ManufacturerPartNumber": params.get('manufacturer_part', ''),
-    #     "Capacitance": params['capacitance'],
-    #     "VoltageRating": params['voltage'],
-    #     "Tolerance": params.get('tolerance', '±20%'),
-    #     "CategoryIds": [56],  # Aluminum Electrolytic Capacitors category
-    #     "RecordCount": 1
-    # }
-
-    data = {
-        "Keywords": str(params['capacitance']) + " " + str(params['voltage']),
-        "Limit": 1,
-        # "Offset": 0,
-        "FilterOptionsRequest": {
-            # "ManufacturerFilter": [ { "Id": "string" } ],
-            "CategoryFilter": [ { "Id": "58" } ],
-            'Manufacturer': [ {'Name': 'Nichicon'} ],
-            # "StatusFilter": [ { "Id": "string" } ],
-            # "PackagingFilter": [ { "Id": "string" } ],
-            # "MarketPlaceFilter": "NoFilter",
-            # "SeriesFilter": [ { "Id": "string" } ],
-            "MinimumQuantityAvailable": 1,
-            # "ParameterFilterRequest": {
-                # "CategoryFilter": { "Id": "56" },
-                # "ParameterFilters": [ {
-                        # "ParameterId": 0,
-                        # "FilterValues": [ { "Id": "string" } ]
-                # } ]
-            # },
-            # "SearchOptions": [ "ChipOutpost" ]
-        },
-        # "SortOptions": {
-        #     "Field": "None",
-        #     "SortOrder": "Ascending"
-        # }
+    
+    FILTER_IDS = {
+        "Packaging": {  "Radial, Can" : "392320", "Axial" : "392322" },
+        "Polarization": { "Polar": "388275", "Non-Polar": "388276" },
+        "Mounting": { "Through Hole": "411897", "Surface Mount": "411898" },
+        "Lead Spacing": { "0.138\" (3.50mm)": "11374", "0.197\" (5.00mm)": "11375" },
+        "Height": { "0.591\" (13.00mm)": "26001", "0.315\" (8.00mm)": "26000" },
+        "Diameter": { "0.315\" Dia (8.00mm)": "18775", "0.394\" Dia (10.00mm)": "18776" },
+        "Lifetime": { "7000 Hrs @ 105°C": "272852", "1000 Hrs @ 105°C": "272853" },
+        "Manufacturer": { "Nichicon": "493", "Panasonic": "10", "Vishay BC Components": "111" }
     }
 
-    print(data)
+    data = {
+        # "Keywords": str(params['capacitance']) + " " + str(params['voltage']),
+        "Limit": 10,
+        "FilterOptionsRequest": {
+            "ManufacturerFilter": [ { "Id": "493" } ], # Nichicon
+            "CategoryFilter": [ { "Id": "58" } ], # Aluminum Electrolytic Capacitors
+            "MarketPlaceFilter": "ExcludeMarketPlace",
+            "MinimumQuantityAvailable": 1,
+            "ParameterFilterRequest": {
+                "CategoryFilter": { "Id": "58" },
+                "ParameterFilters": [ {
+                    "ParameterId": 16, # Package / Case
+                    "FilterValues": [ { "Id": "392320" } ] },{ # Radial, Can
+                    "ParameterId": 2049, # Capacitance
+                    "FilterValues": [ { "Id": "100 uF" } ] }, {
+                    "ParameterId": 2079, # Voltage - Rated
+                    "FilterValues": [ { "Id": "50 V" } ] }, {
+                    # "ParameterId": 725, # Lifetime @ Temp
+                    # "FilterValues": [ { "Id": "272852" } ] }, { # 7000 Hrs @ 105°C
+                    "ParameterId": 52, # Polarization
+                    "FilterValues": [ { "Id": "388275" } ] }, { # Polar
+                    "ParameterId": 69, # Mounting Type
+                    "FilterValues": [ { "Id": "411897" } ] }, # { # Through Hole
+                    # "ParameterId": 508, # Lead Spacing
+                    # "FilterValues": [ { "Id": "11374" } ] }, { # 0.138" (3.50mm)
+                    # "ParameterId": 1500, # Height - Seated (Max) 
+                    # "FilterValues": [ { "Id": "26001" } ] }, { # 0.591" (13.00mm)
+                    # "ParameterId": 46, # Size / Dimension  
+                    # "FilterValues": [ { "Id": "18775" } ] } # 0.315" Dia (8.00mm)
+                ]
+            },
+            "SearchOptions": [ "InStock" ]
+        },
+        "SortOptions": {
+            "Field": "Price",
+            "SortOrder": "Ascending"
+        }
+    }
     
     response = session.post(url=f"{API_BASE}{SEARCH_API}", 
-                            headers={"X-DIGIKEY-Client-Id": CLIENT_ID, "Authorization": session.token['access_token'] },
+                            headers={"X-DIGIKEY-Client-Id": CLIENT_ID, 
+                                     "Authorization": session.token['access_token'] },
                             json=data)
     response.raise_for_status()
     results = response.json()
-    print(results)
+    
+    # pprint(results)
+    print(json.dumps(results, indent=3))
     
     if results.get('Products'):
         return results['Products'][0]['DigiKeyPartNumber']
